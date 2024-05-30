3f0e698708da7262dd41ffcc64763b5b6062bde9
Abdykali Azat <azat.abdykalli@gmail.com>
2024-05-30 14:10:35 +0500

    client 3.1 zhibek zholy

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://cargozhibekzholy.kz:3001',
+    
+    nameCargo: 'Cargo Zhibek Zholy'
+  };
+  
+  
+export default config;
+  
